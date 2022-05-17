f5cc769792a40a390cc5ae1b38d5ac6f8ccc3db5
StradeUni <enrico.stradiotto@studenti.unitn.it>
2022-05-17 16:09:40 +0200

    feat : added cerca campi per nome/luogo/raggio routers

--- api.js
+++ api.js
@@ -58,7 +58,7 @@ app.put('/api/v1/gestore/:idGestore/campo/:idCampo', function(req, res) {
         res.json({"success":false, "message":"Not all required fields were given."})
     }
     
-});
+})
 
 function checkCampoProperties(reqBody){
     return reqBody.nome != undefined && reqBody.nome != null &&
@@ -70,3 +70,33 @@ function checkCampoProperties(reqBody){
     reqBody.tariffa != undefined && reqBody.tariffa != null &&
     reqBody.prenotaEntro != undefined && reqBody.prenotaEntro != null
 }
+
+
+//router cerca campi per nome 
+app.get('/api/v1/CampiPerNome', (req, res) => {
+
+    model.getCampiPerNome(req.body.nomeCampo).then((campi)=>{
+        res.send(JSON.stringify(campi))
+    }).catch(err=>{
+        response.send("error")
+    })
+})
+// router cerca campi per luogo 
+app.get('/api/v1/CampiPerluogo', (req, res) => {
+
+    model.getCampiPerLuogo(req.body.luogo).then((campi)=>{
+        res.send(JSON.stringify(campi))
+    }).catch(err=>{
+        res.send("error")
+    })
+})
+
+// router cerca campi in un  raggio
+app.get('/api/v1/CampiEntroRaggio', (req, res) => {
+
+    model. getCampiNelRAggio(req.body.lat_utente, req.body.long_utente, req.body.raggio).then((campi)=>{
+        res.send(JSON.stringify(campi))
+    }).catch(err=>{
+        res.send("error")
+    })
+});
