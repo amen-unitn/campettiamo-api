5c2099ce6372cbea58575ee87cceb79512902916
Nicola Bernardi <36733534+nicolabernardi@users.noreply.github.com>
2022-05-19 17:25:54 +0200

    Feature/slots+prenotazioni (#6)
    * feat: add support for slots (adding/viewing/deleting) + create prenotazione
    
    * chore: 
    realignment of api endpoints
    
    * chore: tweak endpoints + add some checks before querying db
    
    * fix: get campi by luogo and by coordinates
    
    Co-authored-by: Mattia2700 <mattia2700franzin@gmail.com>

--- api.js
+++ api.js
@@ -7,95 +7,232 @@ var db = require('./db-model');
 const model = new db.Model();
 
 
-app.get('/api/v1/campi', function(req, res) {
-    model.getListaCampi().then((campi)=>{
+app.get('/api/v1/campi', function (req, res) {
+    model.getListaCampi().then((campi) => {
         res.json(campi)
     })
 });
 
-app.get('/api/v1/campo/:id', function(req, res) {
-    model.getCampo(req.params.id).then((result)=>{
+app.get('/api/v1/campo/:id', function (req, res) {
+    model.getCampo(req.params.id).then((result) => {
         res.json(result)
     })
 });
 
-app.delete('/api/v1/gestore/:idGestore/campo/:idCampo', function(req, res) {
-    model.deleteCampo(req.params.idGestore, req.params.idCampo).then((result)=>{
-        if(result)
-            res.json({success:result, message:"Deleted"})
+app.delete('/api/v1/campo/:id', function (req, res) {
+    model.deleteCampo(req.params.id).then((result) => {
+        if (result)
+            res.json({ success: result, message: "Deleted" })
         else
-            res.json({success:result, message:"Campo not found"})
+            res.json({ success: result, message: "Campo not found" })
     })
 });
 
-app.post('/api/v1/gestore/:idGestore/campo/', function(req, res) {
-    if(checkCampoProperties(req.body)){
-        model.createCampo(req.params.idGestore, req.body.nome, req.body.indirizzo, req.body.cap,
-            req.body.citta, req.body.provincia, req.body.sport, req.body.tariffa, req.body.prenotaEntro).then((result)=>{
-            res.json({id:result})
-        })
-    }else{
-        res.json({"success":false, "message":"Not all required fields were given."})
+app.post('/api/v1/campo/', function (req, res) {
+    if (checkCampoProperties(req.body)) {
+        model.createCampo(req.body.idGestore, req.body.nome, req.body.indirizzo, req.body.cap,
+            req.body.citta, req.body.provincia, req.body.sport, req.body.tariffa, req.body.prenotaEntro).then((result) => {
+                res.json({ id: result })
+            })
+    } else {
+        res.json({ "success": false, "message": "Not all required fields were given." })
     }
-    
+
 });
 
-app.put('/api/v1/gestore/:idGestore/campo/:idCampo', function(req, res) {
-    if(checkCampoProperties(req.body)){
-        model.editCampo(req.params.idGestore, req.params.idCampo, req.body.nome, req.body.indirizzo, req.body.cap,
-            req.body.citta, req.body.provincia, req.body.sport, req.body.tariffa, req.body.prenotaEntro).then((result)=>{
-            if(result)
-                res.json({success:result, message:"Updated"})
-            else
-                res.json({success:result, message:"Campo not found"})
-        })
-    }else{
-        res.json({"success":false, "message":"Not all required fields were given."})
+app.put('/api/v1/campo/:id', function (req, res) {
+    if (checkCampoProperties(req.body)) {
+        model.editCampo(req.params.id, req.body.nome, req.body.indirizzo, req.body.cap,
+            req.body.citta, req.body.provincia, req.body.sport, req.body.tariffa, req.body.prenotaEntro).then((result) => {
+                if (result)
+                    res.json({ success: result, message: "Updated" })
+                else
+                    res.json({ success: result, message: "Campo not found" })
+            })
+    } else {
+        res.json({ "success": false, "message": "Not all required fields were given." })
     }
-    
-})
 
-function checkCampoProperties(reqBody){
+});
+
+// slots
+
+app.post('/api/v1/campo/:idCampo/slot', function (req, res) {
+    if (checkSlotProperties(req.body)) {
+        data = model.createSlot(req.params.idCampo, req.body.data, req.body.oraInizio, req.body.oraFine).then((result) => {
+            if (result)
+                res.json({ success: result, message: "Slot created" })
+            else
+                res.json({ success: result, message: "Slot overlaps with another" })
+        })
+    } else {
+        res.json({ "success": false, "message": "Not all required fields were given." })
+    }
+});
+
+app.delete('/api/v1/campo/:idCampo/slot', function (req, res) { // add oraInizio and oraFine
+    if (checkSlotProperties(req.body)) {
+        model.deleteSlot(req.params.idCampo, req.body.data, req.body.oraInizio, req.body.oraFine).then((result) => {
+            res.json({ success: result.success, message: result.message })
+        })
+    } else {
+        res.json({ "success": false, "message": "Not all required fields were given." })
+    }
+});
+
+app.get('/api/v1/campo/:idCampo/slots', function (req, res) {
+    model.getSlots(req.params.idCampo).then((result) => {
+        res.json(result)
+    })
+});
+
+app.get('/api/v1/campo/:idCampo/slot/:data', function (req, res) {
+    let [anno, mese, giorno] = req.params.data.split('-')
+    // check if giorno is passed or not
+    if (giorno == undefined) {
+        model.checkMonthAvailability(req.params.idCampo, mese, anno).then((result) => {
+            res.json(result)
+        })
+    } else {
+        model.getAvailableSlots(req.params.idCampo, req.params.data).then((result) => {
+            res.json(result)
+        })
+    }
+});
+
+// put method is not implemented because it wouldn't be useful specify both old and new values
+// you can delete the old slot and create a new one
+
+app.post('/api/v1/campo/:idCampo/prenotazione', function (req, res) {
+    if (checkPrenotazioneProperties(req.body)) {
+        model.newPrenotazione(req.body.idUtente, req.params.idCampo, req.body.data, req.body.oraInizio, req.body.oraFine).then((result) => {
+            if (result)
+                res.json({ success: true, message: "Prenotazione created", id: result })
+            else
+                res.json({ success: false, message: "Cannot create prenotazione" })
+        })
+    } else {
+        res.json({ "success": false, "message": "Not all required fields were given." })
+    }
+});
+
+app.get('/api/v1/utenti', function (req, res) {
+    model.idUtenti().then((utenti) => {
+        res.json(utenti)
+    })
+});
+
+function checkSlotProperties(reqBody) {
+    return reqBody.data != undefined && reqBody.data != null &&
+        reqBody.oraInizio != undefined && reqBody.oraInizio != null &&
+        reqBody.oraFine != undefined && reqBody.oraFine != null
+}
+
+function checkPrenotazioneProperties(reqBody) {
+    return reqBody.idUtente != undefined && reqBody.idUtente != null &&
+        reqBody.data != undefined && reqBody.data != null &&
+        reqBody.oraInizio != undefined && reqBody.oraInizio != null &&
+        reqBody.oraFine != undefined && reqBody.oraFine != null
+}
+
+function checkCampoProperties(reqBody) {
     return reqBody.nome != undefined && reqBody.nome != null &&
-    reqBody.indirizzo != undefined && reqBody.indirizzo != null &&
-    reqBody.cap != undefined && reqBody.cap != null &&
-    reqBody.citta != undefined && reqBody.citta != null &&
-    reqBody.provincia != undefined && reqBody.provincia != null &&
-    reqBody.sport != undefined && reqBody.sport != null &&
-    reqBody.tariffa != undefined && reqBody.tariffa != null &&
-    reqBody.prenotaEntro != undefined && reqBody.prenotaEntro != null
+        reqBody.indirizzo != undefined && reqBody.indirizzo != null &&
+        reqBody.cap != undefined && reqBody.cap != null &&
+        reqBody.citta != undefined && reqBody.citta != null &&
+        reqBody.provincia != undefined && reqBody.provincia != null &&
+        reqBody.sport != undefined && reqBody.sport != null &&
+        reqBody.tariffa != undefined && reqBody.tariffa != null &&
+        reqBody.prenotaEntro != undefined && reqBody.prenotaEntro != null &&
+        reqBody.idGestore != undefined && reqBody.idGestore != null
 }
 
 
 //router cerca campi per nome 
-app.get('/api/v1/CampiPerNome', (req, res) => {
+app.get('/api/v1/campi-nome', (req, res) => {
 
-    model.getCampiPerNome(req.body.nomeCampo).then((campi)=>{
-        res.send(JSON.stringify(campi))
-    }).catch(err=>{
-        response.send("error")
+    model.getCampiPerNome(req.query.nome).then((campi) => {
+        res.json(campi)
+    }).catch(err => {
+        res.json({success:false, message:"Error"})
     })
 })
-// router cerca campi per luogo 
-app.get('/api/v1/CampiPerluogo', (req, res) => {
+// router cerca campi per luogo (trova prima le coordinate geografiche del luogo)
+app.get('/api/v1/campi-luogo', async (req, res) => {
+	
+    if(req.query.luogo == undefined || req.query.luogo == null || req.query.raggio == undefined || req.query.raggio == null || isNaN(parseFloat(req.query.raggio))){
+    	res.json({success:false, message:"Luogo or raggio not provided"})
+    }else{
+    	coord = await model.getCoordinates(req.query.luogo)
 
-    model.getCampiPerLuogo(req.body.luogo).then((campi)=>{
-        res.send(JSON.stringify(campi))
-    }).catch(err=>{
-        res.send("error")
-    })
+        model.getCampiNelRaggio(coord.lat, coord.lng, parseFloat(req.query.raggio)).then((campi) => {
+            res.json(campi)
+        }).catch(err => {
+            res.json({success:false, message:"Error"})
+        })
+    }
+
 })
 
-// router cerca campi in un  raggio
-app.get('/api/v1/CampiEntroRaggio', (req, res) => {
+// router cerca campi in un raggio
+app.get('/api/v1/campi-raggio', (req, res) => {
 
-    model. getCampiNelRAggio(req.body.lat_utente, req.body.long_utente, req.body.raggio).then((campi)=>{
-        res.send(JSON.stringify(campi))
-    }).catch(err=>{
-        res.send("error")
-    })
+    lat = parseFloat(req.query.lat)
+    lng = parseFloat(req.query.lng)
+    raggio = parseFloat(req.query.raggio)
+    
+    if(isNaN(lat) || isNaN(lng) || isNaN(raggio)){
+    	res.json({success:false, message:"Error on finding data"})
+    }else{
+    	model.getCampiNelRaggio(parseFloat(req.query.lat), parseFloat(req.query.lng), parseFloat(req.query.raggio)).then((campi) => {
+            res.json(campi)
+        }).catch(err => {
+            res.json({success:false, message:"Error"})
+        })
+    }
+
 })
 
+// const test = () => {
+//     let slot = {
+//         oraInizio: "12:00",
+//         oraFine: "16:00",
+//     }
+//     let prenotazioni = [
+//         {
+//             oraInizio: "13:00",
+//             oraFine: "14:00",
+//         },
+//         {
+//             oraInizio: "14:30",
+//             oraFine: "15:30",
+//         }
+//     ]
+//     let slotLiberi = []
+//     prenotazioni = prenotazioni.sort((a, b) => {
+//         return a.oraInizio - b.oraInizio
+//     })
+//     let lastInizio = slot.oraInizio
+//     for (let i = 0; i <= prenotazioni.length; i++) {
+//         if (i == prenotazioni.length && lastInizio != slot.oraFine) {
+//             slotLiberi.push({
+//                 oraInizio: lastInizio,
+//                 oraFine: slot.oraFine
+//             })
+//         } else {
+//             if (lastInizio != prenotazioni[i].oraInizio) {
+//                 slotLiberi.push({
+//                     oraInizio: lastInizio,
+//                     oraFine: prenotazioni[i].oraInizio
+//                 })
+//             }
+//             lastInizio = prenotazioni[i].oraFine
+//         }
+//     }
+//     console.log(slotLiberi)
+// }
+
+// test()
 
 // used for testing
 module.exports = app
---
+++ slot.js
@@ -0,0 +1,9 @@
+class Slot {
+    constructor(data, oraInizio, oraFine) {
+        this.data = data;
+        this.oraInizio = oraInizio;
+        this.oraFine = oraFine;
+    }
+}
+
+module.exports.Slot = Slot;
